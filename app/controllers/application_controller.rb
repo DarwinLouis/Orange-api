7e8603e297d4a8cd68c4140f643448483203c26b
bt <bernardotolosajr@yahoo.com>
2014-02-13 10:43:51 +0800

    clean up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
 	before_filter :authenticate_user_from_token!
 
 	# This is Devise's authentication
-	#before_filter :authenticate_user!
+	before_filter :authenticate_user!
 
 	def authenticate_user_from_token!
 		Rails.logger.info params
